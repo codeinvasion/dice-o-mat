9f955bc7c89a7a5bb5113d3322cd4d26ec931fd7
Friedemann Stoffregen <stoffregen@mankind-software.de>
2015-11-09 11:48:07 +0100

    testing

---
+++ software/node/pin.js
@@ -0,0 +1,20 @@
+var gpio = require('rpi-gpio');
+var pin=null;
+
+module.exports {
+	PIN = function(pin){
+		gpio.setup(pin, gpio.DIR_OUT, write);
+		this.pin = pin;
+	}
+
+	write = function(val){
+		if(this.pin == null)
+		{
+			console.error("pin is not initialized");
+			return;
+		}
+		gpio.write(this.pin, val, function(err){
+			if (err) throw err;
+		});
+	}
+}
