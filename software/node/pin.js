3dcebe1bb5e2d9aedccb7fcdd9e690c8eb2d0ea3
Friedemann Stoffregen <stoffregen@mankind-software.de>
2015-11-09 11:59:07 +0100

    testing

--- software/node/pin.js
+++ software/node/pin.js
@@ -1,11 +1,9 @@
 var PIN = function(pinToSetup){
-	var pin = null;
-
    	var gpio = require('rpi-gpio');
 	gpio.setup(pinToSetup, gpio.DIR_OUT, write);
-	this.pin = pinToSetup;
 
-	var write = function(val){
+	this.pin = pinToSetup;
+	this.write = function(val){
 		if(this.pin == null)
 		{
 			console.error("pin is not initialized");
