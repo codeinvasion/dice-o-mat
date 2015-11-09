51bb4b604fc43d2e9cee9a3468c78ef0fd7ee314
Friedemann Stoffregen <stoffregen@mankind-software.de>
2015-11-09 11:44:42 +0100

    testing

--- software/node/app.js
+++ software/node/app.js
@@ -10,6 +10,6 @@ var gpio = require('rpi-gpio');
 var pinLib = require('./pin');
 var pins = {};
 for(value in setup){
-	pins[value] = new pinLib(setup[value]);
+	pins[value] = new pinLib.PIN(setup[value]);
 	pins[value].write(false);
 }
